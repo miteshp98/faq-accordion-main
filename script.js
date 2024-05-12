04c88c6b033e2aea0b4ef90278a41a7a30b1ef3f
Mitesh Panchal <miteshpanchal458@gmail.com>
2024-05-13 01:33:06 +0530

    use EventListener to Expand Faq

--- script.js
+++ script.js
@@ -1,23 +1,17 @@
 "use strict";
 
-const expand = document.querySelectorAll(".expand");
-const faqAnswer = document.querySelector(".hidden");
-const icon = document.querySelector(".icon");
+document.addEventListener("DOMContentLoaded", function () {
+  const expandButtons = document.querySelectorAll(".expand");
 
-function expandMore() {
-  faqAnswer.classList.remove("hidden");
-  icon.setAttribute("src", "assets/images/icon-minus.svg");
-}
+  expandButtons.forEach(function (button) {
+    button.addEventListener("click", function () {
+      const answer = button.nextElementSibling;
+      const icon = button.lastElementChild;
 
-function expandLess() {
-  faqAnswer.classList.add("hidden");
-  icon.setAttribute("src", "assets/images/icon-plus.svg");
-}
-
-expand.forEach((item) => {
-  item.addEventListener("click", expandMore);
-});
-
-expand.forEach((item) => {
-  item.removeEventListener("click", expandLess);
+      answer.classList.toggle("hidden");
+      icon.src = answer.classList.contains("hidden")
+        ? "/assets/images/icon-plus.svg"
+        : "/assets/images/icon-minus.svg";
+    });
+  });
 });
